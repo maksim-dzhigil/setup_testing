0a43446d74416ff5916bd230cb6d3ac5720ce714
K-mixam <maxdzhigil@gmail.com>
2022-09-12 20:58:29 +0500

    trying to unpack archive

--- tests/unit_tests.py
+++ tests/unit_tests.py
@@ -1,7 +1,14 @@
 import unittest
+import os
 
 class MyTestCase(unittest.TestCase):
+
     def test_something(self):
         self.assertEqual(True, True)
+
     def test_another_something(self):
         self.assertEqual(True, True)
+
+    def test_unpacking_at_ai_ad(self):
+        self.name_list = os.listdir('./acdc_for_test')
+        self.assertIsNotNone(self.name_list)
