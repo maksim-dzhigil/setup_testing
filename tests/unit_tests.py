3df488caaa2cf1a8a7e691e707d0551d32b21aa8
K-mixam <maxdzhigil@gmail.com>
2022-09-12 17:42:26 +0500

    break tests

--- tests/unit_tests.py
+++ tests/unit_tests.py
@@ -2,6 +2,6 @@ import unittest
 
 class MyTestCase(unittest.TestCase):
     def test_something(self):
-        self.assertEqual(True, True)
+        self.assertEqual(False, True)
     def test_another_something(self):
         self.assertEqual(True, True)
