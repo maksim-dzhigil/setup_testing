2e46098ebe5537ae13ba3cfbb60157f6da05057d
K-mixam <maxdzhigil@gmail.com>
2022-09-12 13:31:04 +0500

    prepare for ci testing

--- setup.py
+++ setup.py
@@ -1,7 +1,23 @@
 from distutils.core import setup
 
+
+def read(*names, **kwargs):
+    with io.open(
+        os.path.join(os.path.dirname(__file__), *names),
+        encoding=kwargs.get("encoding", "utf8"),
+    ) as fp:
+        return fp.read()
+
+
+def find_requirements(*file_path):
+    requirements = read(*file_path).split("\n")
+    return [req for req in requirements if req]
+
+requirements = find_requirements("requirements.txt")
+
 setup(
     name="testprojlib",
-    packages=['main', 'main.utils'],
-    url='https://github.com/maksim-dzhigil/setup_testing'
+    packages=['project', 'project.utils'],
+    url='https://github.com/maksim-dzhigil/setup_testing',
+    install_requires = requirements
 )
