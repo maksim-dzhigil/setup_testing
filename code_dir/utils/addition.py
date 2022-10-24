e3cbcdc7510599e6accf58118aa8f17493c13c0f
Maksim Dzhigil <maxdzhigil@gmail.com>
2022-10-24 22:27:57 +0500

    check pre-commit

--- code_dir/utils/addition.py
+++ code_dir/utils/addition.py
@@ -1,8 +1,10 @@
 def sum_(a, b):
-    return a+b 
+    return a + b
 
 
-def  testing_precommit_workability():
-    return  0
+def testing_precommit_workability():
+    return 0
 
 
+def some_func():
+    return 0
